901ff66019077b8d180f6affe236bd0da6efa2ca
am52saif <aliasgar.s.merchant@gmail.com>
2016-03-31 03:36:03 +0000

    Debug

---
+++ debug01.rb
@@ -0,0 +1,5 @@
+list = {'yvr' => 'Vancouver', 'yba' => 'Banff', 'yyz' => 'Toronto', 'yxx' => 'Abbotsford', 'ybw' => 'Calgary'}
+
+# Why is it returning nil instead of first element of the list above
+puts list.keys[0]
+puts list.values[0]
---
+++ debug03.rb
@@ -0,0 +1,20 @@
+def sum(list)
+	sum = 0
+	#Using Each to implement sum
+  list.each do |ele|
+    sum += ele
+  end
+  sum
+#Using Enumerable to implement sum
+  list.map{|x| sum+=x}
+  sum
+end
+
+list1 = [16,21,31,42,55]
+
+# 1. The following should return 165 instead of an error
+puts sum(list1)
+
+# 2. How would you refactor it using an enumerable method other than each? Examples of enumerables: map, select, inject, reject, detect.
+
+
